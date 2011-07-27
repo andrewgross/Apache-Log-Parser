c0fd03095697457d4e7d090a1d512df5a438c8a7
Andrew Gross <agross@qhperform.com>
2011-07-27 16:15:13 -0400

    Adding README, fixing shebang line on parse.py

--- parse.py
+++ parse.py
@@ -1,17 +1,10 @@
+#!/bin/python
 from sys import argv
 from log_object import *
 
 script, input_file = argv
 
-#print "Reading in from %s" % input_file
-
-# Time to service (tts)
-#tts = re.compile('\s\d*$')
-#http_action= re.compile('\"(GET|POST)\s.*\s(HTTP\/1\.\d)\"')
-
 f=open(input_file)
-
-i=1
 
 for line in f:
     log_line = Log_object(line)    
@@ -27,8 +20,3 @@ for line in f:
     print "Time To Service: %s" % log_line.time_to_service
     
         
-# read in each line
-# parse for url
-# parse for time to server
-# save data
-# go to next line
