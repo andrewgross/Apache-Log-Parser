99708e5060079572aac474cff5f9b7a18bf85b8e
Andrew Gross <agross@qhperform.com>
2011-07-27 16:50:07 -0400

    Fixing some 'None' handling issues

--- log_object.py
+++ log_object.py
@@ -25,31 +25,64 @@ class Log_object:
         
     def __parse_ip_address(self, log_line):
         self.ip_address = re.findall('^(?:[0-9]{1,3}\.){3}[0-9]{1,3}', log_line) 
+        if not self.ip_address
+            self.ip_address = "-"
 
     def __parse_date(self, log_line):
         request = re.search('- - \[(.*)\] "', log_line)
-        self.log_date = request.group(1)
+        if request.group[1]:
+            self.log_date = request.group(1)      
+        else:
+            self.log_date = "-"
         
     def __parse_ua_and_tts(self, log_line):
         request = re.search('"\s"(.*)"\s(\d{0,10})$', log_line)
-        self.user_agent = request.group(1)
-        self.time_to_service = request.group(2)
+        if request.group[1]:
+            self.user_agent = request.group(1)      
+        else:
+            self.user_agent = "-"
+        if request.group[2]:
+            self.time_to_service = request.group(2)      
+        else:
+            self.time_to_service = "-"
         
     def __parse_request(self, log_line):
         request = re.search('"(GET|POST|HEAD|PUT|DELETE) (.*)\??.* (HTTP/1.\d)"', log_line)
-        self.http_method = request.group(1)
-        self.request_url = request.group(2)
-        self.http_version = request.group(3)
+        if request.group[1]:
+            self.http_method = request.group(1)      
+        else:
+            self.http_method = "-"
+            
+        if request.group[2]:
+            self.request_url = request.group(2)      
+        else:
+            self.request_url = "-"
+            
+        if request.group[3]:
+            self.http_version = request.group(3)      
+        else:
+            self.http_version = "-"
         
     def __parse_http_code_size(self, log_line):
         request = re.search('HTTP/1.\d" (\d{3}|-) (\d*|-) "', log_line)
-        self.http_code = request.group(1)
-        self.response_size = request.group(2)
+        
+        if request.group[1]:
+            self.response_size = request.group(1)      
+        else:
+            self.response_size = "-"
+        
+        if request.group[2]:
+            self.response_size = request.group(2)       
+        else:
+            self.response_size = "-"
+
         
     def __parse_referrer(self, log_line):
-        request = re.search('\d{3} (\d{0,10}|-) "(.*)" "', log_line)
-        self.referrer = request.group(2)        
-                
+        request = re.search('\d{3} \d*|- "(.*)" "', log_line)
+        if request.group[1]:
+            self.referrer = request.group(1)        
+        else:
+            self.referrer = "-"
         
         
         
