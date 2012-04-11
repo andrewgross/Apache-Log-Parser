275952d0b2dd81e0f2b299b7b3247a9b877880d1
Andrew Gross <andrew@yipit.com>
2012-04-11 17:29:35 -0400

    Psuhing junk

--- functions.py
+++ functions.py
@@ -1,34 +1,30 @@
-from math import sqrt
-from math import pow
 import re
 
-def parse(line):
-    regex = re.compile("^(?P<ip_address>([0-9]{1,3}\.?){4})\s-\s-\s\[(?P<timestamp>.*?)\]\s\"(?P<method>.*?)\s(?P<request>.*?)\sHTTP\/(?P<http_version>.*?)\"\s(?P<response_code>\d{3})\s(?P<size>.*?)\s\"(?P<referrer>.*?)\"\s\"(?P<client>.*?)\"\s(?P<service_time>.*?)$")    
+def parse(line, regex):
     r = regex.search(line)
-    s = {}
+    result_set = {}
     if r:
         for k, v in r.groupdict().iteritems():
             if v is None or v is "-":
                 continue
-            s[k] = r.groupdict().pop(k)
-    return s    
-    # 
-    # def std_dev(aggregate_log_object):        
-    #     average = calculate_mean_time_to_service(aggregate_log_object)
-    #     for tts in aggregate_log_object.aggregate_tts:
-    #         delta_tts.append(pow((average - tts), 2))
-    #     return sqrt(sum(delta_tts)/len(delta_tts))
-    # 
-    # def get_max_tts(aggregate_log_object):
-    #     sorted_aggregate_tts = sorted(aggregate_log_object.aggregate_tts)
-    #     return sorted_aggregate_tts[-1]
-    # 
-    # def get_min_tts(aggregate_log_object):
-    #     sorted_aggregate_tts = sorted(aggregate_log_object.aggregate_tts)
-    #     return sorted_aggregate_tts[0]
-    
+            result_set[k] = r.groupdict().pop(k)
+    return result_set
+
+def parse_apache_combined(line):
+    regex = re.compile("^(?P<ip_address>([0-9]{1,3}\.?){4})\s-\s-\s\[(?P<timestamp>.*?)\]\s\"(?P<method>.*?)\s(?P<request>.*?)\sHTTP\/(?P<http_version>.*?)\"\s(?P<response_code>\d{3})\s(?P<size>.*?)\s\"(?P<referrer>.*?)\"\s\"(?P<client>.*?)\"\s(?P<service_time>.*?)$")    
+    return parse(line, regex) 
 
 
+def parse_nginx(line):
+    regex = re.compile("(?P<ip_address>\S*)\s-\s(?P<requesting_user>\S*)\s\[(?P<timestamp>.*?)\]\s\s\"(?P<method>\S*)\s*(?P<request>\S*)\s*(HTTP\/)*(?P<http_version>.*?)\"\s(?P<response_code>\d{3})\s(?P<size>\S*)\s\"(?P<referrer>[^\"]*)\"\s\"(?P<client>[^\"]*)\"\s(?P<service_time>\S*)\s(?P<application_time>\S*)")    
+    r = regex.search(line)
+    result_set = {}
+    if r:
+        for k, v in r.groupdict().iteritems():
+            if v is None or v is "-":
+                continue
+            result_set[k] = r.groupdict().pop(k)
+    return result_set   
 
 
 
--- parse.py
+++ parse.py
@@ -1,15 +1,87 @@
 #!/bin/python
-from sys import argv
-from functions import parse
+import sys
+from log_parsing_functions import parse_nginx
+import time
 
-script, input_file = argv
+def get_average_over_interval(requests, total_service_time):
+    if requests > 0:
+        return total_service_time/requests
+    else:
+        return 0
 
-f=open(input_file)
-parsed_log=[]
-count = 0
+def parse(line, parsing_function):
+    return parsing_function(line)
 
-for line in f:
-    parsed_log.append(parse(line))
-    print len(parsed_log)
+def get_epoch_time(parsed_line_timestamp):
+    # Convert the time to a more regular format and convert that to epoch time
+    return int(time.mktime(time.strptime(parsed_line_timestamp, "%d/%b/%Y:%H:%M:%S +0000")))
 
-f.close()
+def save_interval(time):
+    interval_data = time_series_data[time]
+    requests, total_service_time, average_service_time = interval_data
+    total_service_time = interval_data[1]
+    average_service_time = get_average_over_interval(requests, total_service_time)
+    interval_data[2] = average_service_time
+
+def create_interval(time):
+    time_series_data[time] = [0, 0, 0]
+
+def update_interval(time, service_time):
+    interval_data = time_series_data[time]
+    requests, total_service_time, average_service_time = interval_data
+    requests = requests + 1
+    total_service_time = total_service_time + service_time
+    interval_data[0] = requests
+    interval_data[1] = total_service_time
+
+def organize_output(time_series_data, initial_interval_time, interval_size):
+    current_time = initial_interval_time
+    output = None
+
+    while time_series_data[current_time]:
+        if output:
+            output = output + ', %.4f' % time_series_data[current_time][2]
+        else:
+            output = '%.4f' % time_series_data[current_time][2]
+        current_time = current_time + interval_size
+    return output
+
+def main(input_files):
+
+    time_series_data={} # { interval_time => [requests, total_service_time, average_service_time] }
+    interval_size = 300 # 5 minutes is our time bucket for now
+
+    current_interval_time = None
+    initial_interval_time = None # Save this so we can interate in order later
+
+    for input_file in input_files:
+        f=open(input_file)
+        for line in f:
+            parsed_line = parse(line, parse_nginx)
+            request_time = get_epoch_time(parsed_line['timestamp'])
+            service_time = float(parsed_line('service_time'))
+            while True:
+                if not current_interval_time:
+                    initial_interval_time = request_time
+                    current_interval_time = request_time
+                    create_interval(current_interval_time)
+
+                if request_time - current_interval_time > interval_size:
+                    save_interval(current_interval_time)
+                    current_interval_time = current_interval_time + interval_size
+                    create_interval(current_interval_time)
+                    continue # because we can have blank intervals from bad data
+
+                update_interval(current_interval_time, service_time)
+                break # Trying to think of a better way to handle blank intervals than 'retry until break'
+
+        save_interval(current_interval_time)
+        f.close()
+
+    print "[%s]" % organize_output(time_series_data, initial_interval_time, interval_size)
+
+if __name__ == '__main__':
+    input_files = sys.argv[1:]
+    main(input_files)
+
+
