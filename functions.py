6e7706422cab51afdfddb0c8cab5da1049994da3
Andrew Gross <andrew@yipit.com>
2012-04-21 14:23:37 -0400

    Reworking to feed statsd

--- functions.py
+++ functions.py
@@ -1,20 +1,5 @@
 import re
 
-def parse(line, regex):
-    r = regex.search(line)
-    result_set = {}
-    if r:
-        for k, v in r.groupdict().iteritems():
-            if v is None or v is "-":
-                continue
-            result_set[k] = r.groupdict().pop(k)
-    return result_set
-
-def parse_apache_combined(line):
-    regex = re.compile("^(?P<ip_address>([0-9]{1,3}\.?){4})\s-\s-\s\[(?P<timestamp>.*?)\]\s\"(?P<method>.*?)\s(?P<request>.*?)\sHTTP\/(?P<http_version>.*?)\"\s(?P<response_code>\d{3})\s(?P<size>.*?)\s\"(?P<referrer>.*?)\"\s\"(?P<client>.*?)\"\s(?P<service_time>.*?)$")    
-    return parse(line, regex) 
-
-
 def parse_nginx(line):
     regex = re.compile("(?P<ip_address>\S*)\s-\s(?P<requesting_user>\S*)\s\[(?P<timestamp>.*?)\]\s\s\"(?P<method>\S*)\s*(?P<request>\S*)\s*(HTTP\/)*(?P<http_version>.*?)\"\s(?P<response_code>\d{3})\s(?P<size>\S*)\s\"(?P<referrer>[^\"]*)\"\s\"(?P<client>[^\"]*)\"\s(?P<service_time>\S*)\s(?P<application_time>\S*)")    
     r = regex.search(line)
--- log_object.py
+++
@@ -1,8 +0,0 @@
-import re
-
-    def parse(self, line):
-        regex = re.compile("^(?P<ip_address>([0-9]{1,3}\.?){4})\s-\s-\s\[(?P<timestamp>.*?)\]\s\"(?P<method>.*?)\s(?P<request>.*?)\sHTTP\/(?P<http_version>.*?)\"\s(?P<response_code>\d{3})\s(?P<size>.*?)\s\"(?P<referrer>.*?)\"\s\"(?P<client>.*?)\"\s(?P<service_time>.*?)$")    
-        r = regex.search(line)
-        for k, v in r.groupdict().iteritems():
-                if v is "-" or None:
-                    del r.groupdict()[k]
